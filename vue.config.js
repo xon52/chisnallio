22efed3983bf2600b601212a4d46ec24709b0df4
xon52 <30713944+xon52@users.noreply.github.com>
2020-04-11 13:01:08 +1000

    Trying again

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,9 @@ const plugins = []
 if (process.env.NODE_ENV === 'production') plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }))
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/chisnallio/'
+    : '/',
   outputDir: __dirname + '/docs',
   transpileDependencies: [],
   configureWebpack: {
