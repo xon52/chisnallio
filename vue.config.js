1bd974e75b8727b284c703230600a15ceaa01778
xon52 <30713944+xon52@users.noreply.github.com>
2020-04-11 12:37:28 +1000

    Reverted silliness

--- vue.config.js
+++ vue.config.js
@@ -1,23 +1,12 @@
-const CopyPlugin = require('copy-webpack-plugin')
-const path = require('path')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
-// For copying static files like favicon
-const plugins = [
-  new CopyPlugin([
-    {
-      from: path.join(__dirname, 'example/public'),
-      to: path.join(__dirname, 'docs'),
-      toType: 'dir',
-      ignore: ['index.html', '.DS_Store']
-    }
-  ])
-]
-// Build analysis
+const plugins = []
 if (process.env.NODE_ENV === 'production') plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }))
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/chisnallio/' : '/',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/chisnallio/'
+    : '/',
   outputDir: __dirname + '/docs',
   transpileDependencies: [],
   configureWebpack: {
