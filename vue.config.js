4de9902f85658b568581a73cac9ee075a35e657a
xon52 <30713944+xon52@users.noreply.github.com>
2020-04-11 12:34:18 +1000

    Github Pages fix

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,23 @@
+const CopyPlugin = require('copy-webpack-plugin')
+const path = require('path')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
-const plugins = []
+// For copying static files like favicon
+const plugins = [
+  new CopyPlugin([
+    {
+      from: path.join(__dirname, 'example/public'),
+      to: path.join(__dirname, 'docs'),
+      toType: 'dir',
+      ignore: ['index.html', '.DS_Store']
+    }
+  ])
+]
+// Build analysis
 if (process.env.NODE_ENV === 'production') plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }))
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/chisnallio/'
-    : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/chisnallio/' : '/',
   outputDir: __dirname + '/docs',
   transpileDependencies: [],
   configureWebpack: {
