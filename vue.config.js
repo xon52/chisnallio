9fab2e9d54a7fe4afd3cfba1647ccc76121a66d6
xon52 <30713944+xon52@users.noreply.github.com>
2020-04-11 14:13:01 +1000

    Added dependencies to transpile config

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,12 @@
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 const plugins = []
-if (process.env.NODE_ENV === 'production') plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }))
+// if (process.env.NODE_ENV === 'production') plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }))
 
 module.exports = {
   outputDir: __dirname + '/docs',
-  transpileDependencies: [],
   configureWebpack: {
     plugins
-  }
+  },
+  transpileDependencies: ['x5-notify', 'x5-modal']
 }
