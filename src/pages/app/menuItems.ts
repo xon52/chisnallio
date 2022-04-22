abbac6f96be6dfaf73e550f80f852371569c2254
Keagan Chisnall <keagan@chisnall.io>
2022-04-22 12:59:24 +1000

    Populated Sidebar component

---
+++ src/pages/app/menuItems.ts
@@ -0,0 +1,42 @@
+import { LinkType } from '../../types'
+
+export const pages: LinkType[] = [
+  {
+    label: 'Home',
+    icon: 'fas house',
+    path: '/',
+  },
+  {
+    label: 'About',
+    icon: 'fas circle-user',
+    path: '/about',
+  },
+  {
+    label: 'Skills',
+    icon: 'fas cubes',
+    path: '/skills',
+  },
+  {
+    label: 'My Work',
+    icon: 'fas trophy',
+    path: '/my-work',
+  },
+  {
+    label: 'Contact',
+    icon: 'fas envelope',
+    path: '/contact',
+  },
+]
+
+export const links: LinkType[] = [
+  {
+    label: 'LinkedIn',
+    icon: 'fab linkedin',
+    url: 'https://www.linkedin.com/in/keagan-chisnall/',
+  },
+  {
+    label: 'Github',
+    icon: 'fab github',
+    url: 'https://github.com/xon52',
+  },
+]
