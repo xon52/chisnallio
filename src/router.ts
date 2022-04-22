8afc81c8d98d3aee5e46fa047bcd6b4318bed450
Keagan Chisnall <keagan@chisnall.io>
2022-04-22 12:57:10 +1000

    Updated routes

--- src/router.ts
+++ src/router.ts
@@ -1,17 +1,21 @@
 import { Ref, ref } from 'vue'
 import { createRouter, createWebHistory, RouteLocationNormalized } from 'vue-router'
-import P403 from '@/pages/errors/403/403.vue'
-import P404 from '@/pages/errors/404/404.vue'
-import P500 from '@/pages/errors/500/500.vue'
-import Home from '@/pages/home/Home.vue'
+import P403 from './pages/errors/403/403.vue'
+import P404 from './pages/errors/404/404.vue'
+import P500 from './pages/errors/500/500.vue'
+import Home from './pages/home/Home.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', name: 'home', component: Home, meta: {} },
-    { path: '/403', name: '403', component: P403, meta: {} },
-    { path: '/500', name: '500', component: P500, meta: {} },
-    { path: '/:pathMatch(.*)*', alias: '/404', name: '404', component: P404, meta: {} },
+    { path: '/', name: 'home', component: Home },
+    { path: '/about', name: 'about', component: Home },
+    { path: '/skills', name: 'skills', component: Home },
+    { path: '/my-work', name: 'my-work', component: Home },
+    { path: '/contact', name: 'contact', component: Home },
+    { path: '/403', name: '403', component: P403 },
+    { path: '/500', name: '500', component: P500 },
+    { path: '/:pathMatch(.*)*', alias: '/404', name: '404', component: P404 },
   ],
 })
 
