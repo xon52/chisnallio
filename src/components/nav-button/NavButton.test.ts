d7b4ce63b9c3c373e78f1527381b691bc1c6f9fd
Keagan Chisnall <keagan@chisnall.io>
2022-04-26 06:34:17 +1000

    NavButton.vue

--- src/components/nav-button/NavButton.test.ts
+++ src/components/nav-button/NavButton.test.ts
@@ -1,34 +1,37 @@
 import { beforeEach, describe, expect, test, vi } from 'vitest'
 import NavButtonVue from './NavButton.vue'
-import { render, fireEvent, RenderResult, waitFor } from '@testing-library/vue'
+import { render, fireEvent, RenderResult } from '@testing-library/vue'
+import { LinkType } from '../../types'
+import router from '../../router'
 
 const wrapperClass = 'nav-button-wrapper'
 const iconClass = 'nav-button-icon'
 const labelClass = 'nav-button-label'
 const loadingClass = 'nav-button-loading'
-const icon = 'fas house'
-const label = 'Home'
-const delay = 400
+const link: LinkType = { icon: 'fas house', label: 'Home' }
+const loadingTime = 300
 
-describe(`nav button loads with an animation`, () => {
-  test(`should render the wrapper with a '${loadingClass}' class and remove it after the ${delay}ms delay`, async () => {
+describe.concurrent(`nav button loads with an animation`, () => {
+  test(`should render the wrapper with a '${loadingClass}' class and remove it after the ${loadingTime}ms delay`, async () => {
     vi.useFakeTimers()
-    const renderResult = render(NavButtonVue, { props: { icon, label, delay } })
+    const renderResult = render(NavButtonVue, { props: { link, loadingTime } })
     const wrapperElement = renderResult.container.getElementsByClassName(wrapperClass)[0]
     expect(wrapperElement.classList).toContainEqual(loadingClass)
-    await vi.advanceTimersByTime(delay)
+    await vi.advanceTimersByTime(loadingTime - 1)
+    expect(wrapperElement.classList).toContainEqual(loadingClass)
+    await vi.advanceTimersByTime(1)
     expect(wrapperElement.classList).not.toContainEqual(loadingClass)
     vi.useRealTimers()
   })
 })
 
-describe(`nav button responds to mouseover event`, () => {
+describe.concurrent(`nav button responds to mouseover event`, () => {
   let renderResult: RenderResult
   const getWrapperElement = () => renderResult.container.getElementsByClassName(wrapperClass)[0]
   const getIconElement = () => renderResult.container.getElementsByClassName(iconClass)[0]
   const getLabelElement = () => renderResult.container.getElementsByClassName(labelClass)[0]
   beforeEach(() => {
-    renderResult = render(NavButtonVue, { props: { icon, label, delay } })
+    renderResult = render(NavButtonVue, { props: { link } })
   })
   test(`should render the icon (class: "${iconClass}") and not the label (class: "${labelClass}") initially`, () => {
     expect(getIconElement()).toBeTruthy()
@@ -36,7 +39,39 @@ describe(`nav button responds to mouseover event`, () => {
   })
   test(`should render the label (class: "${labelClass}") and not the icon (class: "${iconClass}") on hover`, async () => {
     await fireEvent.mouseEnter(getWrapperElement())
-    await waitFor(() => expect(getLabelElement()).toBeTruthy())
+    expect(getLabelElement()).toBeTruthy()
     expect(getIconElement()).toBeFalsy()
+  })
+})
+
+describe.concurrent(`nav button handles size props`, () => {
+  test(`should render the wrapper with no size class`, () => {
+    const renderResult = render(NavButtonVue, { props: { link } })
+    const wrapperElement = renderResult.container.getElementsByClassName(wrapperClass)[0]
+    expect(wrapperElement.classList).not.toContainEqual('large')
+  })
+  test(`should render the wrapper with a 'large' class`, () => {
+    const renderResult = render(NavButtonVue, { props: { link, large: true } })
+    const wrapperElement = renderResult.container.getElementsByClassName(wrapperClass)[0]
+    expect(wrapperElement.classList).toContainEqual('large')
+  })
+})
+
+describe.concurrent(`nav button responds to click event`, () => {
+  test(`should use router.push() when a path is provided in the link`, async () => {
+    router.push = vi.fn()
+    const path = '/test'
+    const renderResult = render(NavButtonVue, { props: { link: { ...link, path } } })
+    const wrapperElement = renderResult.container.getElementsByClassName(wrapperClass)[0]
+    await fireEvent.click(wrapperElement)
+    expect(router.push).toBeCalledWith(path)
+  })
+  test(`should use globalThis.open() when a url is provided in the link`, async () => {
+    globalThis.open = vi.fn()
+    const url = 'https://test.io/url'
+    const renderResult = render(NavButtonVue, { props: { link: { ...link, url } } })
+    const wrapperElement = renderResult.container.getElementsByClassName(wrapperClass)[0]
+    await fireEvent.click(wrapperElement)
+    expect(globalThis.open).toBeCalledWith(url)
   })
 })
--- src/components/nav-button/NavButton.vue
+++ src/components/nav-button/NavButton.vue
@@ -1,56 +1,65 @@
 <template>
   <div
-    :class="['nav-button-wrapper', { 'nav-button-loading': isLoading }]"
+    :class="['nav-button-wrapper', { 'nav-button-loading': isLoading, large: large }]"
     @mouseenter="onHoverChange(true)"
     @mouseleave="onHoverChange(false)"
+    @click="onClick"
   >
     <transition-vue
       mode="out-in"
-      enter-active-class="animated flipInY"
-      leave-active-class="animated flipOutY"
-      :duration="delay"
+      enter-active-class="animate__animated animate__flipInY animate__faster"
+      leave-active-class="animate__animated animate__flipOutY animate__faster"
     >
-      <div v-if="isHover" class="nav-button-label">{{ label }}</div>
-      <fa-icon-wrapper-vue v-else class="nav-button-icon" :icon="icon" />
+      <div v-if="isHover" class="nav-button-label">{{ link.label }}</div>
+      <fa-icon-wrapper-vue v-else class="nav-button-icon" :icon="link.icon" />
     </transition-vue>
   </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref, Transition as TransitionVue } from 'vue'
+import { onMounted, PropType, ref, Transition as TransitionVue } from 'vue'
 import FaIconWrapperVue from '../../dependencies/FaIconWrapper.vue'
+import router from '../../router'
+import { LinkType } from '../../types'
 
-const { icon, label, delay } = defineProps({
-  icon: { type: String, required: true },
-  label: { type: String, required: true },
-  delay: { type: Number, default: 400 },
+const { link, loadingTime } = defineProps({
+  link: { type: Object as PropType<LinkType>, required: true },
+  loadingTime: { type: Number, default: 400 },
+  large: { type: Boolean, default: false },
 })
 
 const isLoading = ref(true)
 const isHover = ref(false)
 const onHoverChange = (hover: boolean) => (isHover.value = hover)
+const onClick = () => {
+  if (link.path) router.push(link.path)
+  else if (link.url) globalThis.open(link.url)
+}
 
-onMounted(() => setTimeout(() => (isLoading.value = false), delay))
+onMounted(() => setTimeout(() => (isLoading.value = false), loadingTime))
 </script>
 
 <style lang="scss" scoped>
 .nav-button-wrapper {
-  height: 50px;
-  width: 50px;
+  height: 20px;
+  padding: 10px;
   display: flex;
   justify-content: center;
   align-items: center;
   cursor: pointer;
   transition: color cubic-bezier(0.7, 0.8, 0.9, 0.95) 0.2s;
+  /* TODO: update sizes with design system vars */
+  font-size: large;
   &:hover {
     color: #ffd700;
   }
-}
-.nav-button-label {
-  font-size: medium;
-}
-.nav-button-icon {
-  font-size: xx-large;
+  &.large {
+    height: 40px;
+    .nav-button-icon {
+      /* TODO: update sizes with design system vars */
+      font-size: xx-large;
+    }
+  }
 }
 .nav-button-loading {
   opacity: 0;
