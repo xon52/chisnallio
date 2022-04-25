f42f3c5a550d22173a1c1a6acc96227de5fefe3e
Keagan Chisnall <keagan@chisnall.io>
2022-04-26 06:33:32 +1000

    Added icons

---
+++ src/dependencies/FaIconWrapper.vue
@@ -0,0 +1,12 @@
+<template>
+  <font-awesome-icon :icon="icon.split(' ')" />
+</template>
+
+<script setup lang="ts">
+import './icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+const { icon } = defineProps({
+  icon: { type: String, required: true },
+})
+</script>
---
+++ src/dependencies/icons.ts
@@ -0,0 +1,9 @@
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import specific icons */
+import { faHouse, faCubes, faEnvelope, faTrophy, faCircleUser } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+
+/* add icons to the library */
+library.add(faHouse, faCubes, faEnvelope, faTrophy, faCircleUser, faLinkedin, faGithub)
